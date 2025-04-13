3a9b94b25ce21c7fc3d262eddd52d7ce6a4269b2
SY-rabbit <sumyeetsy@gmail.com>
2025-04-14 00:00:09 +0100

    activating the search engine

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert("hi");
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${input.value}`;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchFormSubmit);
