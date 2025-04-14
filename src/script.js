25e15708e34a31e06be833799cbc2465b718e921
SY-rabbit <sumyeetsy@gmail.com>
2025-04-14 02:31:15 +0100

    adding functions

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${input.value}`;
+function getWeather(response) {
+  let tempElement = document.querySelector("#current-temp");
+  let CityTemp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(CityTemp);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchFormSubmit);
+function searchCity(city) {
+  let apiKey = "a2t704ade1b003d643fa9333o6bb8862";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+function searchResult(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-input");
+  searchCity(input.value);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", searchResult);
